55a61d5619e597c6ba22743026945e40278f298c
Jonathan Dushoff <dushoff@mcmaster.ca>
2016-05-20 18:22:15 -0400

    Autocommit (scratch) On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   Makefile
    	modified:   johnson.rmd
    	modified:   johnson_test.R
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	commit.time
    	johnson.R.merge
    	johnson.diff
    	johnson.html
    	nlme_cache/

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 ### Hooks for the editor to set the default target
 current: target
 
-target pngtarget pdftarget vtarget acrtarget: johnson.html 
+target pngtarget pdftarget vtarget acrtarget pushtarget: johnson_test.Rout 
 
 ##################################################################
 
--- johnson_test.R
+++ johnson_test.R
@@ -1,33 +1,37 @@
-
-## Parameters for a particular vector
 P <- 0.1
-v <- c(50, 80, 400)
-plim <- 0.01
-boxes <- 5000
+CI <- c(25, 400)
+qlim <- c(10, 1000)
+boxes <- 500
+steps <- 6
 
-pars <- jsqpar(q=v, P=P)
+## Medians
+lCI <- log(CI)
+medlist <- exp(seq(from=min(lCI), to=max(lCI), length.out=steps+1))
+medlist <- medlist[-c(1, steps+1)]
 
-qlim <- (jsqfun(qnorm(qpfun(plim)), pars=pars))
-qlim <- c(10, 500)
-
+## Points of the distribution (and their differences)
 j <- seq(min(qlim), max(qlim), length.out=boxes+1)
-
 del <- (j[-1] - j[-length(j)])
 j <- (j[-1] + j[-length(j)])/2
 
-dens <- jsqdens(j, q=v, P=P)
-plot(j, dens, type="l")
-dd <- (del*dens)
-print(sum(dd))
-print(plim/2+sum(dd[j<v[[1]]]))
-print(plim/2+sum(dd[j<v[[2]]]))
-print(plim/2+sum(dd[j<v[[3]]]))
+for (med in medlist){
+	dens <- jsqdens(j, q=c(CI[[1]], med, CI[[2]]), P=P, logq=TRUE)
+	# plot(j, dens, type="l")
+}
 
-dens <- jsqdens(j, q=v, P=P, logq=TRUE)
-plot(j, dens, type="l")
-dd <- (del*dens)
-print(sum(dd))
-print(plim/2+sum(dd[j<v[[1]]]))
-print(plim/2+sum(dd[j<v[[2]]]))
-print(plim/2+sum(dd[j<v[[3]]]))
 
+### Linear style
+medlist <- seq(from=min(CI), to=max(CI), length.out=steps+1)
+medlist <- medlist[-c(1, steps+1)]
+
+qlim <- c(-50, 500)
+## Points of the distribution (and their differences)
+j <- seq(min(qlim), max(qlim), length.out=boxes+1)
+del <- (j[-1] - j[-length(j)])
+j <- (j[-1] + j[-length(j)])/2
+
+for (med in medlist){
+	print(jsqpar(q=c(CI[[1]], med, CI[[2]]), P=P))
+	dens <- jsqdens(j, q=c(CI[[1]], med, CI[[2]]), P=P)
+	plot(j, dens, type="l")
+}
