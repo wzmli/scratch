cace04ad2254e372dc28adb5489cfb9d1b21e8c2
Jonathan Dushoff <dushoff@mcmaster.ca>
2016-05-23 17:01:53 -0400

    A nice plotting function

--- johnson_test.R
+++ johnson_test.R
@@ -1,37 +1,54 @@
-P <- 0.1
+
+### Linear fits and plots
+seqPlot <- function(CI, qlim, boxes, steps, logq=FALSE, P=0.1, plotlim=NULL){
+	# List of medians
+	medlim <- CI
+	if (logq) medlim <- log(medlim)
+	medlist <- seq(from=min(medlim), to=max(medlim), length.out=steps+1)
+	medlist <- medlist[-c(1, steps+1)]
+	if (logq) medlist <- exp(medlist)
+
+	## Points of the distribution (and their differences)
+	j <- seq(min(qlim), max(qlim), length.out=boxes+1)
+	del <- (j[-1] - j[-length(j)])
+	j <- (j[-1] + j[-length(j)])/2
+
+	dlist <- sapply(medlist, function(med){
+		d <- jsqdens(j, q=c(CI[[1]], med, CI[[2]]), P=P, logq=logq)
+		dd <- del*d
+		print(c(
+			tot = sum(dd)
+			, down = sum(dd[j<=max(CI)])
+			, up = sum(dd[j>=min(CI)])
+		))
+		return(d)
+	})
+
+	matplot(j, dlist, type="l", lwd="3", lty=1, xlim=plotlim)
+	abline(v=CI, lty=3, lwd=1.5)
+	for (i in 1:length(medlist)){
+		abline(v=medlist[[i]], col=i, lty=2, lwd=1.5)
+	}
+}
+
 CI <- c(25, 400)
-qlim <- c(10, 1000)
-boxes <- 500
+P <- 0.1
+
 steps <- 6
+boxes <- 10000
 
-## Medians
-lCI <- log(CI)
-medlist <- exp(seq(from=min(lCI), to=max(lCI), length.out=steps+1))
-medlist <- medlist[-c(1, steps+1)]
+seqPlot (CI = CI
+	, qlim = c(-100, 500)
+	, boxes = boxes
+	, steps = steps
+	, P=P
+)
 
-## Points of the distribution (and their differences)
-j <- seq(min(qlim), max(qlim), length.out=boxes+1)
-del <- (j[-1] - j[-length(j)])
-j <- (j[-1] + j[-length(j)])/2
+seqPlot (CI = CI
+	, qlim = c(10, 600)
+	, boxes = boxes
+	, steps = steps
+	, P=P
+	, logq=TRUE
+)
 
-for (med in medlist){
-	dens <- jsqdens(j, q=c(CI[[1]], med, CI[[2]]), P=P, logq=TRUE)
-	# plot(j, dens, type="l")
-}
-
-
-### Linear style
-medlist <- seq(from=min(CI), to=max(CI), length.out=steps+1)
-medlist <- medlist[-c(1, steps+1)]
-
-qlim <- c(-50, 500)
-## Points of the distribution (and their differences)
-j <- seq(min(qlim), max(qlim), length.out=boxes+1)
-del <- (j[-1] - j[-length(j)])
-j <- (j[-1] + j[-length(j)])/2
-
-for (med in medlist){
-	print(jsqpar(q=c(CI[[1]], med, CI[[2]]), P=P))
-	dens <- jsqdens(j, q=c(CI[[1]], med, CI[[2]]), P=P)
-	plot(j, dens, type="l")
-}
